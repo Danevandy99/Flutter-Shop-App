8cf7fc389bb7ac23fac3cf48e475c9ab1232ea90
Dane Vanderbilt <danevandy@hotmail.com>
2021-09-01 08:35:42 -0500

    End of Section 10

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -12,27 +12,50 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
   @override
   void initState() {
-    super.initState();
+    _ordersFuture = _obtainOrdersFuture();
 
-    Future.delayed(Duration.zero).then((_) {
-      Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-    });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    //final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemBuilder: (_, index) => OrderItem(orderData.orders[index]),
-        itemCount: orderData.orders.length,
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (builderContext, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              return Center(
+                child: Text('An error occurred'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (_, orderData, child) => ListView.builder(
+                  itemBuilder: (_, index) => OrderItem(orderData.orders[index]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
       ),
     );
   }
